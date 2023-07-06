cab54f4f13653de33f678c9c63ac1f77109e04d7
Yoriaki11 <131589067+Yoriaki11@users.noreply.github.com>
2023-07-07 07:34:27 +0900

    ブックマークアプリ課題

---
+++ kadai03/book_db2.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost
+-- 生成日時: 2023 年 7 月 07 日 00:30
+-- サーバのバージョン： 10.4.28-MariaDB
+-- PHP のバージョン: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `book_db2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_an_table`
+--
+
+CREATE TABLE `gs_an_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `url` varchar(256) NOT NULL,
+  `content` text DEFAULT NULL,
+  `indate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_an_table`
+--
+
+INSERT INTO `gs_an_table` (`id`, `name`, `url`, `content`, `indate`) VALUES
+(14, 'はらぺこあおむし', 'google', ' 懐かしい ', '2023-07-06 20:54:22');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
